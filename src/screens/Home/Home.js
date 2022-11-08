7d2f1d547ee3e35a538bf45e358202194b4e2f28
Ariel Verdugo <arielverdugo@MacBook-Pro-de-Ariel-2.local>
2022-11-08 19:48:41 -0300

    Se agrego la logica para el login,obtener el session id y un avance de obtener las peliculas

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,22 +4,37 @@ import { Text, View } from 'react-native';
 import { Config } from 'react-native-config';
 import { useSelector } from 'react-redux';
 import { strings } from '@/localization';
+
 import { getUser } from '@/selectors/UserSelectors';
 import { styles } from '@/screens/Home/Home.styles';
 import { typography } from '@/theme';
+import { UseGetAllmovies } from '@/actions/UserActions';
 
 export function Home() {
   const { colors } = useTheme();
   const user = useSelector(getUser);
+  //const{data,isLoading} = UseGetAllmovies();
 
+  //console.log(data);
   return (
     <View style={styles.container}>
-      <Text style={[typography.title, { color: colors.text }]}>
-        {strings.home.message} {user?.username}
-      </Text>
-      <Text style={[typography.text, { color: colors.text }]}>
-        {strings.home.variant} {Config.BUILD_VARIANT}
-      </Text>
+      
     </View>
   );
 }
+
+/*
+{isLoading ? (
+        <Text>Loading...</Text>
+      ) : results ? (
+        data.map((movie,key) =>{
+          return(
+            <View>
+               <Text>{movie.originalTitle}</Text>
+            </View>
+          );
+        })
+      ): (
+        <Text>No movies</Text>
+      ) }
+      */
